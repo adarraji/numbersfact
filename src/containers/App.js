fa400f559bdf4479bab17d42dec22d115acce0d0
adarraji <a.darraji@outlook.com>
2022-05-03 18:48:24 -0400

    Added contstructor and state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 
 class App extends Component {
-
+    constructor() {
+        super()
+        this.state = {};
+    }
 
     render() {
         return (
