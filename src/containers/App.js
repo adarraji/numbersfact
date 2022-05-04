6ad965d90d1b852793a025681c17c8e12014809d
adarraji <a.darraji@outlook.com>
2022-05-04 16:25:28 -0400

    Added font styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className="georgia f6">
                 <Input />
                 <Output />
             </div>
