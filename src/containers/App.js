6f7d5a7ef9322b6454d1f20517de6f9ca38ca64f
adarraji <a.darraji@outlook.com>
2022-05-04 16:50:03 -0400

    Changed font color

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
     render() {
         return (
-            <div className="georgia f6">
+            <div className="georgia f6 white">
                 <Input />
                 <Output />
             </div>
