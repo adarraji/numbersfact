f3db4fb991d472312e20059539d1432df2f0e5c2
adarraji <a.darraji@outlook.com>
2022-05-03 20:14:37 -0400

    Added Input container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./Input";
 
 class App extends Component {
     constructor() {
@@ -9,7 +10,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello !!!</h1>
+                <Input />
             </div>
         );
     }
---
+++ src/containers/Input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+    return (
+        <div>
+            <h1>Input</h1>
+        </div>
+    );
+}
+
+export default Input
