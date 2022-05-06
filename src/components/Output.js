2ae8bf28eeaa160b60b9dea435d85ed6d058074a
adarraji <a.darraji@outlook.com>
2022-05-05 21:35:24 -0400

    Added styles

--- src/components/Output.js
+++ src/components/Output.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Output = ({ factText }) => {
     return (
-        <div className="m3 mw6 pa3 br3">
+        <div className="m3 mw6 pa3 br3 output">
             {factText}
         </div>
     );
