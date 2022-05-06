ca296823bbbcdb9f1c0d91c83f7ae357027a3930
adarraji <a.darraji@outlook.com>
2022-05-05 20:36:13 -0400

    Added App css file

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,11 @@
 import React from "react";
 import TextBox from "./TextBox";
 import Button from "../components/Button";
+import "./App.css";
 
 const Input = ({ onSubmit, textChnage }) => {
     return (
-        <div className="flex mb3">
+        <div className="flex mb3 input">
             <TextBox textChnage={textChnage} />
             <Button onSubmit={onSubmit} />
         </div>
